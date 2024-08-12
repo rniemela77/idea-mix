ab1154e47eb2ae4df4e22f7029f23c6b3a3b21cb
Robert Niemela <84466763+rniemela77@users.noreply.github.com>
2024-08-12 02:16:25 +0000

    install

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    server: {
+      type: 'https',
+    },
+    port: 8080, // Use the port you want
+    client: {
+      webSocketURL: 'wss://silver-space-capybara-4rxrj6rq6gj2qrrp-8080.app.github.dev/ws', // Use wss for WebSocket Secure
+    },
+  },
+});
