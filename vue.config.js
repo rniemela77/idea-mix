5122ff3e9fb38608a97a452e4928743a4cdf8ee5
Robert Niemela <84466763+rniemela77@users.noreply.github.com>
2024-08-12 02:24:15 +0000

    vue.config update

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,31 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    server: {
+      type: 'https',
+    },
+    port: 8080, // Use the port you want
+    client: {
+      webSocketURL: 'wss://silver-space-capybara-4rxrj6rq6gj2qrrp-8080.app.github.dev/ws', // Use wss for WebSocket Secure
+    },
+  },
   pwa: {
-    name: 'My PWA',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    
-    // configure the workbox plugin
+    name: "My PWA",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
     workboxOptions: {
       skipWaiting: true,
       clientsClaim: true,
       runtimeCaching: [
         {
-          urlPattern: new RegExp('^https://my-api-domain/'),
-          handler: 'NetworkFirst',
+          urlPattern: new RegExp("^https://my-api-domain/"),
+          handler: "NetworkFirst",
           options: {
-            cacheName: 'api-cache',
+            cacheName: "api-cache",
             expiration: {
               maxEntries: 10,
               maxAgeSeconds: 300,
@@ -25,4 +35,4 @@ module.exports = {
       ],
     },
   },
-};
+});
