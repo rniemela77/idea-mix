cfe0fc4ad801d7474ecfd02a2652d559ce65126e
Robert Niemela <84466763+rniemela77@users.noreply.github.com>
2024-08-12 02:32:02 +0000

    tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
+  <div>
+    <h1>Idea-Mix</h1>
+    <i>In development...</i>
+  </div>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+  },
+};
 </script>
 
 <style>
@@ -22,5 +27,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+
+  background: rgba(49, 160, 141, 0.1);
+}
+* {
+  background: rgba(49, 160, 141, 0.1);
 }
 </style>
